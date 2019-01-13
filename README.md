3639f94fc3c350baba8033a2f75d88f7dc502ea7
danielle <dmlicea@gmail.com>
2019-01-12 20:43:13 -0600

    upload

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # unit-4-game
+
+* Crystal Collector is a project meant to test the coder's knowledge of Javascript, particularly jQuery. 
+
+* Crystal Collector takes the form of a game the user can play in their browser. 
+
+* Each time you open the game, the user is also given a random number between 19 and 120.
+
+* There are four buttons, each labeled with a different gem. Each of these buttons is assigned a numberical value between 1 and 12- but unlike the random number, the user can not see this value.
+
+* The user is also given a score that always starts at 0. Each time you click one of the buttons, the user's score will increase by an amount that corresponds to the value assigned to that button. 
+
+* The goal of the game is to try to figure out how to make your score match the random number. Whether the user wins (by making their score exactly equal to the random number) or loses (by making their score higher than the random number), the game will reset afterwards.
+
+* I started by getting all my variables down. To get random values for the buttons, i used the Math.random function with the Math.floor function to make sure all numbers would be whole, and then set it to only get numbers between 1 and 12. I used the same code for the random number, just with a different range.
+
+* The user's score had to increase in a way visible to the user. The same applied to the random number and the amount of wins and loses. I used the .text method in ocnjuction with <span> tags for this to create dynamically updating numbers.
+
+* Each button had an id assigned to it so i could assign a  .on method that activated on a click. This would update the score and run the play() function.
+
+* The play() function checks if the score is equal or higher than the random number. If it finds either of those specifications to be true, it updates either the win or loss counter and runs the newGame function.
+
+* The newGame() function resets all the variables (apart from wins and loses) so the user can start a new game.
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
-//variables
+
+  //variables
 
 var wins = 0;
 var loses = 0;
@@ -25,7 +26,7 @@ console.log(button4)
 console.log(randomNum)
 console.log(yourScore)
 
-//text in html
+//text in html (locates the span id and assign text based on the variable)
 
 $('#wins').text(wins);
 $('#loses').text(loses);
@@ -34,25 +35,14 @@ $('#randomNum').text(randomNum);
 
 
 
-//buttons
+//buttons 
+// (.on Method, on click runs a function that updates your score and updates the score text[see above], runs play)
 
 $("#oneG").on("click", function() {
 
     yourScore = yourScore + button1;
     $('#yourScore').text(yourScore); 
-
-    if (yourScore == randomNum)
-    {
-      youWin();
-    
-    }
-    else if (yourScore > randomNum)
-    {
-      youLose();
-    
-    }
-
-  
+    play()
   });
 
 
@@ -60,55 +50,66 @@ $("#oneG").on("click", function() {
 
     yourScore = yourScore + button2;
     $('#yourScore').text(yourScore); 
-
-    if (yourScore == randomNum)
-    {youWin();}
-    else if (yourScore > randomNum)
-    {youLose();}
-
-  
+    play()
   });
  
   $("#threeG").on("click", function() {
 
     yourScore = yourScore + button3;
     $('#yourScore').text(yourScore);
-
-    if (yourScore == randomNum)
-    {youWin();}
-    else if (yourScore > randomNum)
-    {youLose();}
-
-  
+    play()
   });
 
   $("#fourG").on("click", function() {
 
     yourScore = yourScore + button4;
     $('#yourScore').text(yourScore);
-
-    if (yourScore == randomNum)
-    {youWin();}
-    else if (yourScore > randomNum)
-    {youLose();} 
-
-  
+    play()
   });
+
+
+  // winlose
+  // if statements (if your score is equal, you win, if it's too high, you lose)
+  // either way newGame runs
+
+  var play = function () {
+  if (yourScore === randomNum)
+  {
+    wins++;
+    $("#wins").text(wins);
+    newGame();}
+
+  if (yourScore > randomNum)
+  {loses++;
+    $("#loses").text(loses);
+    newGame();} 
+
+
+// logs
+
+console.clear()
+console.log("yourWins",yourWins)
+console.log("YourLost",yourLoses)
+console.log("wins",wins)
+console.log("loses",loses)
+console.log("randomNum",randomNum)
+console.log("yourScore",yourScore)
+
   
+  }
 
-
-//functions
-
+//functions reset
+//varaibles reset
 
 
 function newGame(){
+yourScore = 0;
+button1 = Math.floor(Math.random() * 12+1);
+button2 = Math.floor(Math.random() * 12+1);
+button3 = Math.floor(Math.random() * 12+1);
+button4 = Math.floor(Math.random() * 12+1);
+randomNum = Math.floor(Math.random() * 120+19);
 
-var button1 = Math.floor(Math.random() * 12+1);
-var button2 = Math.floor(Math.random() * 12+1);
-var button3 = Math.floor(Math.random() * 12+1);
-var button4 = Math.floor(Math.random() * 12+1);
-var randomNum = Math.floor(Math.random() * 120+19);
-var yourScore = 0;
 $('#yourScore').text(yourScore);
 $('#randomNum').text(randomNum);
 
@@ -116,22 +117,12 @@ $('#randomNum').text(randomNum);
 
 
 
-function youWin(){
-  wins++;
-  ('#wins').text(wins);
-  newGame();
-
-}
-function youLose(){
-  loses++;
-  ('#loses').text(loses);
-  newGame();
-
-}
 
 
 
 });
+
+//end
 
 /* Gem Game Pseudocode
 * The player is shown a number
